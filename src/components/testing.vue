b2c5f681d8ed0aa5e9b13871aa232437fd60c3ab
kjb244 <kevin.bacino@gmail.com>
2021-04-14 10:53:38 -0400

    label test

--- src/components/testing.vue
+++ src/components/testing.vue
@@ -3,13 +3,13 @@
         <div class="row">
             <div class="col col-sm-12 col-md-6">
                 <h3 class="text-center">testing</h3>
-                <form>
+                <form autocomplete="off">
                     <b-form-checkbox @change="cbChange()" id="checkbox-1" v-model="cbox" name="checkbox-1">
                         Show/hide
                     </b-form-checkbox>
                     <div v-show="cbox">
                         <label class="mt-2" :for="addressLine1.id">{{addressLine1.label}}</label>
-                        <b-form-input autocomplete="new-password"  :id="addressLine1.id" v-model="addressLine1.model" ></b-form-input>
+                        <b-form-input   :id="addressLine1.id" v-model="addressLine1.model" ></b-form-input>
 
                         <label class="mt-5" :for="addressLine2.id">{{addressLine2.label}} </label>
                         <b-form-input autocomplete="new-password" :id="addressLine2.id" v-model="addressLine2.model"></b-form-input>
@@ -48,12 +48,12 @@
                 addressLine1: {
                     model: '',
                     id: 'addressLine1',
-                    label: 'Line 1'
+                    label: 'address Line 1'
                 },
                 addressLine2: {
                     model: '',
                     id: 'addressLine2',
-                    label: 'Line 2'
+                    label: 'address Line 2'
                 },
                 city: {
                     model: '',
@@ -80,8 +80,8 @@
         methods: {
 
             cbChange: function(){
-                this.addressLine1.id = this.addressLine1.id.split('').reverse().join('');
-                this.addressLine2.id = this.addressLine2.id.split('').reverse().join('');
+                //this.addressLine1.id = this.addressLine1.id.split('').reverse().join('');
+                //this.addressLine2.id = this.addressLine2.id.split('').reverse().join('');
                 this.city.id = this.city.id.split('').reverse().join('');
                 this.state.id = this.state.id.split('').reverse().join('');
                 this.zip.id = this.zip.id.split('').reverse().join('');
