c76c9d1d905d0d1918d02a47baa5628230ab3468
kjb244 <kevin.bacino@gmail.com>
2021-04-22 19:45:07 -0400

    testing street number

--- src/components/testingFormInputs2.vue
+++ src/components/testingFormInputs2.vue
@@ -7,7 +7,7 @@
 
                     <div class="mb-5" v-bind:key="rec.name + rec.id + rec.label" v-for="rec in inputs">
                         <label class="mt-2" :for="rec.id">{{rec.label}}</label>
-                        <b-form-input  :name="rec.name"  :id="rec.id" v-model="rec.model" ></b-form-input>
+                        <b-form-input  :name="rec.name" :class="rec.class || rec.name"  :id="rec.id" v-model="rec.model" ></b-form-input>
                         {{rec}}
                     </div>
 
@@ -36,6 +36,14 @@
         data(){
             return{
                 inputs: [
+
+                    {
+                        model: '',
+                        id: 'streetNumberMailing',
+                        name: 'streetNumberMailing',
+                        class: 'street-number-mailing-field',
+                        label: 'Street Number (optional)',
+                    },
                     {
                         model: '',
                         id: 'addressLine1',
